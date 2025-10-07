cb90bdcd5d6cd7814e7124ae1fda2374149eb10d
hafidha <hafidha.bouabdelli@gmail.com>
2025-10-07 17:08:03 +0100

    security with arcjet

---
+++ lib/arcjet.ts
@@ -0,0 +1,25 @@
+import { env } from "./env";
+// import {shield, slidingWindoe} from "@arjet/next";
+import arcjet, {
+  detectBot,
+  shield,
+  slidingWindow,
+  protectSignup,
+  sensitiveInfo,
+  fixedWindow,
+} from "@arcjet/next";
+export {
+  detectBot,
+  shield,
+  slidingWindow,
+  protectSignup,
+  fixedWindow,
+  sensitiveInfo,
+};
+
+export default arcjet({
+  key: env.ARCJET_KEY,
+  characteristics: ["fingerprint"],
+
+  rules: [shield({ mode: "LIVE" })],
+});
